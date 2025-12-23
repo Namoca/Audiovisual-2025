31daca85a9a52f24088edfafb5eac8264391cc2a
Namoca <natinamoca#gmail.com>
2025-12-23 12:12:33 -0300

    dia2teste2

--- dados.js
+++ dados.js
@@ -43,105 +43,189 @@ const obras = [
         nota: 0.0 
     },
 	{ 
-        tipo: "", 
-        titulo: "", 
-        imagem: "img/", 
+        tipo: "filmes", 
+        titulo: "Entrevista com o Vampiro (1994)", 
+        imagem: "img/entrevista_com.jpeg", 
         descricao: "", 
         nota: 0.0 
     },
     { 
-        tipo: "", 
+        tipo: "filmes", 
+        titulo: "O Grande Gatsby (2013)", 
+        imagem: "img/gatsby.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "Kpop Demon Hunters (2025)", 
+        imagem: "img/kpop.jpg", 
+        descricao: "Fiz ouvindo as músicas", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "filmes", 
+        titulo: "Drácula de Bram Stoker (1992)", 
+        imagem: "img/dracula.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "Blade Runner - O Caçador de Androides (1982)", 
+        imagem: "img/blade_runner.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "filmes", 
+        titulo: "Kung Fury (2015)", 
+        imagem: "img/kung_fury.png", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "O Senhor das Armas (2005)", 
+        imagem: "img/armas.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "filmes", 
+        titulo: "Sonic 3 (2024S)", 
+        imagem: "img/sonic3.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "Cisne Negro (2010)", 
+        imagem: "img/cisne.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "filmes", 
+		titulo: "Truque de Mestre - O 3º Ato (2025)", 
+        imagem: "img/truque.jpeg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "A Entrevista (2014)", 
+        imagem: "img/a_entrevista.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "filmes", 
+        titulo: "Em Ritmo de Fuga (2017)", 
+        imagem: "img/em_ritmo.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "Jurado Nº2", 
+        imagem: "img/jurado.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "filmes", 
+        titulo: "Bingo: O Rei das Manhãs (2017)", 
+        imagem: "img/bingo.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "Orgulho e Preconceito e Zumbis (2016)", 
+        imagem: "img/orgulho.jpeg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "Rivais (2024)", 
+        imagem: "img/rivais.jpeg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "Diário de uma Paixão (2004)", 
+        imagem: "img/diario.jpeg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "A Garota Ideal (2007)", 
+        imagem: "img/a_garota.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "Donnie Darko (2001)", 
+        imagem: "img/donnie.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "Ace Ventura: Um Detetive Diferente (1994)", 
+        imagem: "img/ace.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
+        titulo: "Mickey 17 (2025)", 
+        imagem: "img/mickey.jpeg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "filmes", 
         titulo: "", 
         imagem: "img/", 
         descricao: "", 
         nota: 0.0 
     },
 	{ 
-        tipo: "", 
-        titulo: "", 
-        imagem: "img/", 
-        descricao: "", 
-        nota: 0.0 
-    },
-    { 
-        tipo: "", 
+        tipo: "filmes", 
         titulo: "", 
         imagem: "img/", 
         descricao: "", 
         nota: 0.0 
     },
 	{ 
-        tipo: "", 
-        titulo: "", 
-        imagem: "img/", 
-        descricao: "", 
-        nota: 0.0 
-    },
-    { 
-        tipo: "", 
+        tipo: "filmes", 
         titulo: "", 
         imagem: "img/", 
         descricao: "", 
         nota: 0.0 
     },
 	{ 
-        tipo: "", 
-        titulo: "", 
-        imagem: "img/", 
-        descricao: "", 
-        nota: 0.0 
-    },
-    { 
-        tipo: "", 
+        tipo: "filmes", 
         titulo: "", 
         imagem: "img/", 
         descricao: "", 
         nota: 0.0 
     },
 	{ 
-        tipo: "", 
-        titulo: "", 
-        imagem: "img/", 
-        descricao: "", 
-        nota: 0.0 
-    },
-    { 
-        tipo: "", 
+        tipo: "filmes", 
         titulo: "", 
         imagem: "img/", 
         descricao: "", 
         nota: 0.0 
     },
 	{ 
-        tipo: "", 
-        titulo: "", 
-        imagem: "img/", 
-        descricao: "", 
-        nota: 0.0 
-    },
-    { 
-        tipo: "", 
-        titulo: "", 
-        imagem: "img/", 
-        descricao: "", 
-        nota: 0.0 
-    },
-	{ 
-        tipo: "", 
-        titulo: "", 
-        imagem: "img/", 
-        descricao: "", 
-        nota: 0.0 
-    },
-    { 
-        tipo: "", 
-        titulo: "", 
-        imagem: "img/", 
-        descricao: "", 
-        nota: 0.0 
-    },
-	{ 
-        tipo: "", 
+        tipo: "filmes", 
         titulo: "", 
         imagem: "img/", 
         descricao: "", 

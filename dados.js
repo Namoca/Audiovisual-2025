0fdbcb23b7f3f4183bbe55f2e6fc4594cfa1fbf9
Namoca <natinamoca#gmail.com>
2025-12-23 11:24:55 -0300

    dia2teste1

---
+++ dados.js
@@ -0,0 +1,150 @@
+// Este ficheiro contém apenas a lista de dados (o array de objetos)
+const obras = [
+    { 
+        tipo: "filmes", 
+        titulo: "Um Sonho de Liberdade (1994)", 
+        imagem: "img/um_sonho.png", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "filmes", 
+        titulo: "Parasita (2019)", 
+        imagem: "img/parasita.jpg", 
+        descricao: "", 
+        nota: 0.0
+    },
+    { 
+        tipo: "filmes", 
+        titulo: "O Lobo de Wall Street (2013)", 
+        imagem: "img/o_lobo.jpg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "filmes", 
+        titulo: "Akira (1988)", 
+        imagem: "img/akira.jpg", 
+        descricao: "", 
+        nota: 0.0
+    },
+    { 
+        tipo: "filmes", 
+        titulo: "Pecadores (2025)", 
+        imagem: "img/sinners.jpeg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "filmes", 
+        titulo: "Superman (2025)", 
+        imagem: "img/superman.jpeg", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+    { 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    },
+	{ 
+        tipo: "", 
+        titulo: "", 
+        imagem: "img/", 
+        descricao: "", 
+        nota: 0.0 
+    }
+];
